910e99179fe21f927d00f91e2937c416494fbf77
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-25 08:57:31 +0200

    Initial commit

---
+++ src/com/Popa/Main.java
@@ -0,0 +1,12 @@
+package com.Popa;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Capitals[] = {"Paris", "London", "New York", "Stockholm"};
+        Arrays.sort(Capitals);
+        System.out.println("Sorted string array: " + Arrays.toString(Capitals));
+    }
+}
