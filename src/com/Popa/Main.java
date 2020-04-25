7da9608d7180771f98bdd45a1a8271e0f895e404
The-Popa <tobias.carlstrom@bredband2.com>
2020-04-25 11:21:04 +0200

    Updated version

--- src/com/Popa/Main.java
+++ src/com/Popa/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         String Capitals[] = {"Paris", "London", "New York", "Stockholm"};
+        System.out.println("String array: " + Arrays.toString(Capitals));
         Arrays.sort(Capitals);
         System.out.println("Sorted string array: " + Arrays.toString(Capitals));
     }
